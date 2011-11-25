c397283f8e2444d7756a2787fcfe626c9e246293
sourabh <sourabh@sourabh-VirtualBox.(none)>
2011-11-25 02:52:11 -0500

    Djikstar priority queue Need to add a few more lines to be able to simulate Djikstar

---
+++ Djikstra.c
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+struct node {
+    int nodeNo;
+    float distance;
+    int pred;
+} *elems;
+
+float **adj;
+
+struct node** indexer;
+struct node** heap;
+
+int parent(int i) {
+    if (i==0)
+	return 0;
+    else
+	return (int)((i-1)/2);
+}
+
+int left(int i) {
+    return 2*i+1;
+}
+
+int right(int i) {
+    return 2*i+2;
+}
+
+void minHeapify(struct node **A,int i,int size) {
+    int l=left(i);
+    int r=right(i);
+    int smallest;
+    struct node* temp;
+    if ((l<size)&&((*(A+l))->distance < (*(A+i))->distance)) {
+        smallest=l;
+    } else {
+        smallest=i;
+    }
+    if((r<size)&&((*(A+r))->distance < (*(A+smallest))->distance)) {
+        smallest=r;
+    }
+    if(smallest!=i) {
+        temp=*(A+i);
+        *(A+i)=*(A+smallest);
+        *(A+smallest)=temp;
+        minHeapify(A,smallest,size);
+    }
+}
+
+void buildMinHeap(struct node **A,int size) {
+    int i=size/2;
+    while(i>=0) {
+        minHeapify(A,i,size);       
+	i--;
+    }   
+}
+
+int extractMin(struct node **A,int* size) {
+    struct node* temp;
+    int m=*size-1;
+    temp=*(A+m);
+    *(A+m)=*(A);
+    *(A)=temp;
+    (*size)--;
+    minHeapify(A,0,*size);
+    return (*(A+m))->nodeNo;
+
+}
+
+
+void decreaseKey(struct node *A,struct node **index,int size,int key,int pos) {
+    (A+pos)->distance=key;
+    buildMinHeap(index,size);
+}
+
+int main() {
+    int size=10;  
+    int test[10]={8,9,6,3,4,9,2,1,5,2};
+    elems=(struct node*)malloc(10*sizeof(struct node));
+    heap=(struct node**)malloc(10*sizeof(struct node*));
+    int iter=0;
+    while(iter < 10) {
+        *(heap+iter)=(elems+iter);
+        (elems+iter)->distance=test[iter];
+        (elems+iter)->nodeNo=iter;
+        iter++;
+    }
+   int i=0;
+   while(i<size) {
+        printf("The min element is : %f from heap pointers\n",(*(heap+i))->distance);
+        i++;
+   }
+
+   buildMinHeap(heap,10);
+   i=0;
+   int posMin;
+   printf("\n\n");
+   while(size !=0) {
+        posMin=extractMin(heap,&size);
+       // printf("The min element is : %f from heap pointers\n",(*(heap+i))->distance);
+        printf("The minimum value from extractMin is : %f\n",(elems+posMin)->distance);
+        //i++;
+   }
+   i=0;
+   printf("\n\n");
+   /*
+   while(i<size) {
+        printf("The min element is : %f from heap pointers\n",(*(heap+i))->distance);
+        i++;
+   }
+   */
+   /*
+   decreaseKey(elems,heap,size,1,1);
+   i=0;
+   printf("\n\n");
+   while(i<size) {
+        printf("The min element is : %f from heap pointers\n",(*(heap+i))->distance);
+        i++;
+   }
+   */
+}
